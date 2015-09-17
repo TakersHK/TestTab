4d120978f7cd958d069ad29d663568e56d81a8d5
TakersHK <colman.ng@hotmail.com>
2015-09-17 19:38:23 +0800

    Show map name and position marker to center. marker have latency.

--- app/src/main/java/com/inspiritstudio/testtab/MapFragment.java
+++ app/src/main/java/com/inspiritstudio/testtab/MapFragment.java
@@ -5,6 +5,7 @@ import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -35,11 +36,15 @@ import java.util.List;
 import java.util.Locale;
 
 public class MapFragment extends Fragment implements GoogleMap.OnMarkerDragListener {
-
-
+//public class MapFragment extends Fragment {
+    private MyLocationListener locationListener;
+    private LocationManager locationManager;
     MapView mMapView;
     private GoogleMap googleMap;
-
+    private String provider;
+    private double latitude;
+    private double longitude;
+    private Location myLocation;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -58,6 +63,119 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerDragListe
         }
 
         googleMap = mMapView.getMap();
+        // Enable MyLocation Layer of Google Map
+        googleMap.setMyLocationEnabled(true);
+
+
+
+        // Get LocationManager object from System Service LOCATION_SERVICE
+        //LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+           // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        //String provider = locationManager.getBestProvider(criteria, true);
+        provider = locationManager.getBestProvider(criteria, true);
+        // Get Current Location
+
+        locationManager.requestLocationUpdates(provider, 0, 0,
+                new LocationListener() {
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+                    }
+
+                    @Override
+                    public void onLocationChanged(final Location location) {
+                    }
+                });
+         myLocation  = locationManager
+                .getLastKnownLocation(provider);
+
+
+        //Location myLocation = locationManager.getLastKnownLocation(provider);
+
+        // +
+
+
+        //-
+
+        // set map type
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        latitude =  myLocation.getLatitude();
+        longitude = myLocation.getLongitude();
+
+        // Get latitude of the current location
+        //LatLng place = myLocation.getLatitude();
+        LatLng place = new LatLng(latitude,longitude);
+        //LatLng place = new LatLng(22.3199, 114.1714);
+        CameraPosition cameraPosition =
+                new CameraPosition.Builder()
+                        .target(place)
+                        .zoom(17)
+                        .build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+//map marker start +
+
+        googleMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            public void onCameraChange(CameraPosition arg0) {
+                googleMap.clear();
+                int maxResults = 1;
+                try {
+                    Geocoder gc = new Geocoder(getActivity(), Locale.getDefault());
+                    LatLng markerPosition = new MarkerOptions().position(arg0.target).getPosition();
+
+                    //List<Address> addresses = gc.getFromLocation(latitude,longitude, maxResults);
+                    List<Address> addresses = gc.getFromLocation(markerPosition.latitude,markerPosition.longitude, maxResults);
+                    googleMap.addMarker(new MarkerOptions().position(arg0.target).title("You are here").snippet(addresses.get(0).toString())).showInfoWindow();
+
+                    Toast.makeText(getActivity().getApplicationContext(),addresses.get(0).toString() , Toast.LENGTH_LONG).show();
+                    addresses.clear();
+                }
+                catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    //Log.e("Canont get Address!");
+                }
+
+            }
+        });
+
+//map marker end-
+/*
+        int maxResults = 1;
+        try {
+            Geocoder gc = new Geocoder(getActivity(), Locale.getDefault());
+            List<Address> addresses = gc.getFromLocation(myLocation.getLatitude(),myLocation.getLongitude(), maxResults);
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
+            googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!").snippet(addresses.get(0).toString()));
+            /*longitude
+            if (addresses.size() == 1) {
+                return addresses.get(0);
+            } else {
+                return null;
+            }
+
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            //Log.e("Canont get Address!");
+        }
+*/
+
+        /*
         // latitude and longitude
         double latitude = 17.385044;
         double longitude = 78.486671;
@@ -75,7 +193,7 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerDragListe
         // Enable MyLocation Layer of Google Map
         googleMap.setMyLocationEnabled(true);
 
-        googleMap.setOnMarkerDragListener(this);
+       // googleMap.setOnMarkerDragListener(this);
 
         //test+
         // Get LocationManager object from System Service LOCATION_SERVICE
@@ -118,13 +236,14 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerDragListe
             } else {
                 return null;
             }
-            */
+
         }
         catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
             //Log.e("Canont get Address!");
         }
+        */
         // Zoom in the Google Map
        // googleMap.animateCamera(CameraUpdateFactory.zoomTo(17));
         //googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title("You are here!").snippet("Consider yourself located"));
@@ -141,22 +260,56 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerDragListe
         return v;
     }
 
+
+    class MyLocationListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            //if (location != null) {
+            //    loc.setText(LocationSample.showLocation(location));
+            //} else {
+            //    loc.setText("Cannot get location!");
+            //}
+        }
+        public void onProviderDisabled(String provider) {
+        }
+        public void onProviderEnabled(String provider) {
+        }
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
     @Override
     public void onResume() {
         super.onResume();
+/*
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
+        if (provider != null) {
+            locationManager.requestLocationUpdates(provider, 1000, 1, locationListener);
+        } else {
+            locationManager.requestLocationUpdates
+                    (LocationManager.GPS_PROVIDER, 0, 1, locationListener);
+        }
+*/
+
         mMapView.onResume();
+
+
+
     }
 
     @Override
     public void onPause() {
         super.onPause();
         mMapView.onPause();
+        locationManager.removeUpdates(locationListener);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
+        locationManager.removeUpdates(locationListener);
+        locationManager.setTestProviderEnabled(provider, false);
     }
 
     @Override
@@ -164,7 +317,6 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerDragListe
         super.onLowMemory();
         mMapView.onLowMemory();
     }
-
     @Override
     public void onMarkerDragStart(Marker marker) {
 
@@ -183,4 +335,5 @@ public class MapFragment extends Fragment implements GoogleMap.OnMarkerDragListe
         Log.i("info", "on drag end :" + dragLat + " dragLong :" + dragLong);
         Toast.makeText(getActivity().getApplicationContext(), "Marker Dragged..!", Toast.LENGTH_LONG).show();
     }
+
 }
